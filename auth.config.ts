b243fcc95019df08a9f22dff1e9e35e3f89d77e7
Huang Wei Wei <vxoweiwei@gmail.com>
2024-02-13 21:53:11 +0800

    Configure nextauth v5 beta

--- auth.config.ts
+++ auth.config.ts
@@ -1,15 +1,11 @@
-import { NextAuthOptions } from "next-auth";
 import GithubProviver from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 import { prisma } from "@/lib/prisma";
+import type { NextAuthConfig } from "next-auth";
 
-export const options: NextAuthOptions = {
-    // adapter: PrismaAdapter(Prisma) as Adapter,
-    pages: {
-        signIn: '/auth/signin',
-    },
+export default {
     providers: [
         CredentialsProvider({
             name: 'Credentials',
@@ -25,13 +21,13 @@ export const options: NextAuthOptions = {
                 try {
                     const foundUser = await prisma.user.findUnique({
                         where: {
-                            email: credentials?.email,
+                            email: credentials?.email as string,
                         },
                     });
                     if (foundUser) {
                         console.log('User exist');
                         const passwordMatch = await bcrypt.compare(
-                            (credentials?.password as string), foundUser.password
+                            (credentials?.password as string), foundUser.password!
                         );
 
                         if (passwordMatch) {
@@ -53,19 +49,19 @@ export const options: NextAuthOptions = {
         GithubProviver({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
-            profile(profile) {
-                console.log("Profile Github: ", profile);
+            // profile(profile) {
+            //     console.log("Profile Github: ", profile);
 
-                let userRole = "GitHub User"
+            //     let userRole = "GitHub User"
 
-                if(profile?.email == "vxoweiwei@gmail.com") {
-                    userRole = "admin";
-                }
+            //     if(profile?.email == "vxoweiwei@gmail.com") {
+            //         userRole = "admin";
+            //     }
 
-                return {
-                    ...profile, role: userRole,
-                }
-            }
+            //     return {
+            //         ...profile, role: userRole,
+            //     }
+            // }
         }),
         GoogleProvider({
             clientId: process.env.GOOGLE_ID as string,
@@ -87,14 +83,4 @@ export const options: NextAuthOptions = {
             }
         }),
     ],
-    callbacks: {
-        async jwt({ token, user }: any) {
-            if (user) token.role = user.role
-            return token
-        },
-        async session({ session, token }: any) {
-            if (session?.user) session.user.role = token.role
-            return session
-        }
-    }
-}
+} satisfies NextAuthConfig;
--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,29 @@
-import { withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
 
+export const { auth: middleware } = NextAuth(authConfig);
 
-export default withAuth(
-    function middleware(req) {
-        console.log(req.nextUrl.pathname);
-        console.log(req.nextauth.token);
+// export default withAuth(
+//     function middleware(req) {
+//         console.log(req.nextUrl.pathname);
+//         console.log(req.nextauth.token);
 
-        if (
-            req.nextUrl.pathname.startsWith('/admin') &&
-            req.nextauth.token?.role != "admin"
-        ) {
-            return NextResponse.rewrite(new URL('/denied', req.url));
-        }
-    },
-    {
-        pages: {
-            signIn: '/auth/signin',
-        },
-        callbacks: {
-            authorized: ({ token }) => !!token,
-        }
-    }
-)
+//         if (
+//             req.nextUrl.pathname.startsWith('/admin') &&
+//             req.nextauth.token?.role != "admin"
+//         ) {
+//             return NextResponse.rewrite(new URL('/denied', req.url));
+//         }
+//     },
+//     {
+//         pages: {
+//             signIn: '/auth/signin',
+//         },
+//         callbacks: {
+//             authorized: ({ token }) => !!token,
+//         }
+//     }
+// )
 
 export const config = {
     matcher: ['/admin']
